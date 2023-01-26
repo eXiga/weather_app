b16baaf1f01b74fbb42b1d288c2a7994e54bd4ea
eXiga <anton.kostenich@hotmail.com>
2023-01-26 20:06:42 +0100

    start implementation of settings screen

--- wapp/components/SettingsButton.tsx
+++ wapp/components/SettingsButton.tsx
@@ -1,15 +1,50 @@
-import { Pressable, StyleSheet } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
 
-export default function SettingsButton() {
-  return <Pressable style={styles.button}></Pressable>;
+interface SettingsButtonProps {
+  title: string;
+  value: string;
+  onPress: () => void;
+}
+
+export default function SettingsButton(props: SettingsButtonProps) {
+  return (
+    <Pressable style={styles.button} onPress={props.onPress}>
+      <View style={styles.container}>
+        <Text style={styles.title}>{props.title}</Text>
+        <View style={styles.valueContainer}>
+          <Text style={styles.value}>{props.value}</Text>
+          <MaterialIcons name="keyboard-arrow-right" size={24} color="black" />
+        </View>
+      </View>
+    </Pressable>
+  );
 }
 
 const styles = StyleSheet.create({
   button: {
-    height: 75,
+    height: 60,
     width: "100%",
-    backgroundColor: "red",
-    borderColor: "black",
-    marginTop: 1,
+  },
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginLeft: 20,
+  },
+  valueContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  title: {
+    fontFamily: "Montserrat-Light",
+    fontWeight: "300",
+    fontSize: 15,
+  },
+  value: {
+    fontFamily: "Montserrat-Thin",
+    fontWeight: "400",
+    fontSize: 13,
   },
 });
--- wapp/pages/Settings.tsx
+++ wapp/pages/Settings.tsx
@@ -1,17 +1,63 @@
-import { Text, View } from "react-native";
+import { SafeAreaView, View, StyleSheet } from "react-native";
+import {
+  createStackNavigator,
+  StackNavigationProp,
+} from "@react-navigation/stack";
 import Theme from "../design/Design";
+import { useNavigation } from "@react-navigation/native";
+import { SettingsButton } from "../components";
+
+type SettingsScreens = {
+  Main: undefined;
+};
+
+type StackScreenNavigationProp = StackNavigationProp<SettingsScreens>;
+
+const SettingsStack = createStackNavigator<SettingsScreens>();
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Theme.backgroundColor,
+  },
+});
+
+function MainSettings() {
+  const navigation = useNavigation<StackScreenNavigationProp>();
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={{ flex: 1, flexDirection: "column", marginTop: 20 }}>
+        <SettingsButton
+          title="City"
+          value="undefined"
+          onPress={() => console.log("CITY")}
+        />
+        <SettingsButton
+          title="Temperature"
+          value="Fahrenheit"
+          onPress={() => console.log("TEMP")}
+        />
+        <SettingsButton
+          title="Measurements"
+          value="Imperial"
+          onPress={() => console.log("METRIC")}
+        />
+      </View>
+    </SafeAreaView>
+  );
+}
 
 export default function SettingsScreen() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: Theme.backgroundColor,
-      }}
-    >
-      <Text>Settings!</Text>
-    </View>
+    <SettingsStack.Navigator>
+      <SettingsStack.Screen
+        name="Main"
+        component={MainSettings}
+        options={{ headerShown: false }}
+      />
+    </SettingsStack.Navigator>
   );
 }
+
+/// https://open-meteo.com/
